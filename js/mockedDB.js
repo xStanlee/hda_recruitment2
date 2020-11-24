3e305768e4dd6849b9f5053d399d6d5f72f01d9d
Patryk Stachura <57445724+xStanlee@users.noreply.github.com>
2020-11-24 10:06:41 +0100

    Mocked database
    Database mocked.

---
+++ js/mockedDB.js
@@ -0,0 +1,15 @@
+// Mocked db
+
+const db = {
+    people: [
+        { name: 'John', age: 27 },
+        { name: 'Jack', age: 19 },
+        { name: 'Mack', age: 51 },
+        { name: 'Sasin', age: 70 },
+        { name: 'Richard', age: 34 },
+        { name: 'Andrew', age: 42 },
+    ]
+}
+
+//module.exports = { db };
+export { db }
